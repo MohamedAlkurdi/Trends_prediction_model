a346097885e6cb5465504d83646faad676c1f083
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2024-10-29 23:40:45 +0300

    visualized `entities count over time` statistic

--- ner_attempt.py
+++ ner_attempt.py
@@ -1,8 +1,6 @@
 import spacy
 import pandas as pd
 
-# entity_counts_every_day_output = r"C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.txt"
-
 nlp = spacy.load("en_core_web_sm")
 data = pd.read_csv(r"C:\Users\alkrd\Desktop\graduation_project\the_project\preprocssed_data\cleaned_data_USA.csv")
 
@@ -22,23 +20,24 @@ for _, row in dataFrame.iterrows():
         'entities': entities 
     })
 
+
+#------------------------------------------------
 df_entities = pd.DataFrame(entities_data)
 
 df_entities['date'] = pd.to_datetime(df_entities['date'])
 
-# Expand the 'entities' list into individual rows for counting
 exploded_df = df_entities.explode('entities')
 
-# Group by date and entity type, then count occurrences
 entity_counts_every_day = exploded_df.groupby(['date', 'entities']).size().unstack(fill_value=0)
 
-# Display the result to show entity frequencies over time
-
-print(entity_counts_every_day)
-
-
 entity_counts_every_day.to_csv(r'C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.csv', header=True, index=True)
-entity_counts_every_day.to_csv(r'C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.txt', header=True, index=True)
+
+
+#------------------------------------------------
+
+df_entities_traffic = pd.DataFrame(entities_data)
+
+df_entities_traffic['traffic'] = pd.to_numeric(df_entities_traffic['traffic'])
 
 
 
---
+++ visualize_entity_count_everyday.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+entity_counts_over_time = pd.read_csv(r'C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.csv')
+
+entity_counts_over_time[['CARDINAL', 'DATE', 'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'MONEY', 'NORP', 'ORDINAL', 'ORG', 'PERCENT', 'PERSON', 'PRODUCT', 'QUANTITY', 'TIME', 'WORK_OF_ART']].plot(kind='line', figsize=(12, 6))
+
+plt.title('Entity Frequency Over Time')
+plt.xlabel('Date')
+plt.ylabel('Frequency')
+plt.legend(title='Entity Type')
+plt.show()
