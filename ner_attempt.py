1c1aa14f7d6a4111e48d7c96d296a9fed1ca64cb
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2024-10-29 23:26:49 +0300

    performed an operation to calculate the entities frequency over time (entity counts everyday)

--- ner_attempt.py
+++ ner_attempt.py
@@ -1,6 +1,44 @@
 import spacy
+import pandas as pd
+
+# entity_counts_every_day_output = r"C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.txt"
+
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Classicalcrossover singer Jackie Evancho will sing The Star Spangled Banner at Donald Trumps inauguration ceremony on Friday January 20th The 16yearold who was the runnerup on 2010s Americas Got Talent competition broke the news")
-for ent in doc.ents:
-    print(ent.text, "|", ent.label_, "|", spacy.explain(ent.label_))
-print(nlp.pipe_labels['ner'])
+data = pd.read_csv(r"C:\Users\alkrd\Desktop\graduation_project\the_project\preprocssed_data\cleaned_data_USA.csv")
+
+dataFrame = data[['index', 'date', 'traffic', 'newsTitle', 'newsSnippet']]
+
+entities_data = []
+
+for _, row in dataFrame.iterrows():
+    doc = nlp(row['newsTitle'] + " " + row['newsSnippet'])
+    
+    entities = [ent.label_ for ent in doc.ents]
+    
+    entities_data.append({
+        'id': row['index'],
+        'date': row['date'],
+        'traffic': row['traffic'],
+        'entities': entities 
+    })
+
+df_entities = pd.DataFrame(entities_data)
+
+df_entities['date'] = pd.to_datetime(df_entities['date'])
+
+# Expand the 'entities' list into individual rows for counting
+exploded_df = df_entities.explode('entities')
+
+# Group by date and entity type, then count occurrences
+entity_counts_every_day = exploded_df.groupby(['date', 'entities']).size().unstack(fill_value=0)
+
+# Display the result to show entity frequencies over time
+
+print(entity_counts_every_day)
+
+
+entity_counts_every_day.to_csv(r'C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.csv', header=True, index=True)
+entity_counts_every_day.to_csv(r'C:\Users\alkrd\Desktop\graduation_project\the_project\entity_counts_every_day.txt', header=True, index=True)
+
+
+
