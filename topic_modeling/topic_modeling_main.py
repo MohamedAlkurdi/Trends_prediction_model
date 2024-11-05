d8d718a0969562ab95094823960538d0c56ff53d
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2024-11-05 23:50:40 +0300

    last update for the current report (8)

--- topic_modeling/topic_modeling_main.py
+++ topic_modeling/topic_modeling_main.py
@@ -2,17 +2,15 @@ import pandas as pd
 import gensim
 import gensim.corpora as corpora
 from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel
-
-import pyLDAvis
-import pyLDAvis.gensim
+from gensim.models import TfidfModel
 
 import spacy
 from nltk.corpus import stopwords
 
+import pyLDAvis
+import pyLDAvis.gensim
+
 english_stopwords = stopwords.words("english")
-
-
 data = pd.read_csv(r"C:\Users\alkrd\Desktop\graduation_project\the_project\preprocssed_data\cleaned_data_USA.csv")
 dataFrame = data[['newsSnippet']]
 
@@ -22,15 +20,13 @@ def lemmatization(texts, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
     text_out = []
     for text in texts:
         doc = nlp(text)
-        # print("\ndoc: ", doc)
         new_text = [token.lemma_ for token in doc if token.pos_ in allowed_postags]
         final = " ".join(new_text)
         text_out.append(final)
     return text_out
 
-
 lemmatized_texts = lemmatization(dataFrame['newsSnippet'].astype(str).tolist())
-print("\n lemmitized sample: ", lemmatized_texts[100])
+print("lemmitized sample: ", lemmatized_texts[100])
 
 def gen_words(texts):
     print("removing stop words...")
@@ -43,13 +39,51 @@ def gen_words(texts):
 data_words = gen_words(lemmatized_texts)
 print("after removing stop words: ",data_words[100])
 
-id2word = corpora.Dictionary(data_words)
-corpus = []
-print("vectorizing...")
-for text in data_words:
-    new = id2word.doc2bow(text)
-    corpus.append(new)
-print("corpus? ", corpus[10:15])
+
+
+bigrams_phrases = gensim.models.Phrases(data_words, min_count=3, threshold=80)
+trigram_phrases = gensim.models.Phrases(bigrams_phrases[data_words], threshold=80)
+
+bigram = gensim.models.phrases.Phraser(bigrams_phrases)
+trigram = gensim.models.phrases.Phraser(trigram_phrases)
+
+def make_bigrams(texts):
+    return(bigram[doc] for doc in texts)
+
+def make_tigram(texts):
+    return (trigram[bigram[doc]] for doc in texts)
+
+data_bigrams = list(make_bigrams(data_words))
+data_bigrams_trigrams = list(make_tigram(data_bigrams))
+
+print(data_bigrams_trigrams)
+
+
+
+id2word = corpora.Dictionary(data_bigrams_trigrams)
+texts = data_bigrams_trigrams
+corpus = [id2word.doc2bow(text) for text in texts]
+print(corpus[0])
+
+tfidf = TfidfModel(corpus, id2word=id2word)
+
+low_value = 0.03
+words = []
+words_missing_in_tfidf = []
+
+for i in range(0, len(corpus)):
+    bow = corpus[i]
+    low_value_words = []
+    tfidf_ids = [id for id, value in tfidf[bow]]
+    bow_ids = [id for id, value in bow]
+    low_value_words = [id for id, value in tfidf[bow] if value < low_value]
+    drops = low_value_words+words_missing_in_tfidf
+    for item in drops:
+        words.append(id2word[item])
+    words_missing_in_tfidf = [id for id in bow_ids if id not in tfidf_ids] # The words with tf-idf socre 0 will be missing
+
+    new_bow = [b for b in bow if b[0] not in low_value_words and b[0] not in words_missing_in_tfidf]  
+    corpus[i] = new_bow
 
 lda_model = gensim.models.ldamodel.LdaModel(
     corpus=corpus,
@@ -58,6 +92,6 @@ lda_model = gensim.models.ldamodel.LdaModel(
     random_state=100,
     update_every=1,
     chunksize=100,
-    passes=10,
+    passes=20,
     alpha="auto"
 )
